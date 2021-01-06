0740c5b95e7f7ff2ba1a9898158e501ac5b0adab
jeddou <jeddou.ehtp@gmail.com>
2021-01-06 13:51:07 +0100

    create django views to render python objects

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'jeddou',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2020'
-    },
-    {
-        'author': 'Medo van',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
