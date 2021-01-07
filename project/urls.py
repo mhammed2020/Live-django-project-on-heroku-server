97fd00cb0b970da7f93cfeab5371030f99d46467
jeddou <jeddou.ehtp@gmail.com>
2021-01-07 21:13:14 +0100

    add MEDIA_URL to main urls.py

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
