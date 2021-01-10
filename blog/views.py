1aeea39fb0fb07cc7b20e88c86e8593d5d2dd831
jeddou <jeddou.ehtp@gmail.com>
2021-01-10 21:54:12 +0100

    create django forms for comment model

--- blog/views.py
+++ blog/views.py
@@ -37,8 +37,41 @@ class UserPostListView(ListView):
         return Post.objects.filter(author=user).order_by('-date_posted')
 
 
-class PostDetailView(DetailView):
-    model =  Post
+# class PostDetailView(DetailView):
+#     model =  Post
+
+from .forms import NewComment
+
+def post_detail(request,post_id) :
+    
+    post = get_object_or_404(Post,pk=post_id)
+    comments = post.comments.all()
+    # contributors = User.objects.all()
+   
+
+   
+    if request.method == 'POST' :
+        comment_form = NewComment(data = request.POST)
+
+        if comment_form.is_valid() :
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            comment_form = NewComment()
+
+    else :
+        comment_form = NewComment()
+    context ={
+        'title':post,
+        'post' : post,
+        'comments':comments,
+        'comment_form' :comment_form, 
+    }
+
+
+   
+    return render(request,'blog/post_detail.html', context)
+
 
 
 class PostCreateView(LoginRequiredMixin,CreateView):
--- project/settings.py
+++ project/settings.py
@@ -139,8 +139,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'thenewcasablanca2.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'sttjwxplhbamvoji'
+EMAIL_HOST_USER = 'pop.tac@gmail.com'
+EMAIL_HOST_PASSWORD = '****'
 
 # EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
