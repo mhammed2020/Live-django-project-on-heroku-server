e44fb0d6fdd24096ad9d85c4d1c34b66f31bdb54
jeddou <jeddou.ehtp@gmail.com>
2021-01-10 12:43:31 +0100

    using  ListView  cbvs

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+
 from .models import Post
 
 
@@ -8,6 +17,11 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'  
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
