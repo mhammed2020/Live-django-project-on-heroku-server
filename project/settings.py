fc6b048925540099b39d5cafa340ad22fa771ac4
jeddou <jeddou.ehtp@gmail.com>
2021-01-10 17:56:05 +0100

    add template to render django messages

--- project/settings.py
+++ project/settings.py
@@ -139,5 +139,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '***.gdr@gmail.com'
-EMAIL_HOST_PASSWORD = '*****8'
+EMAIL_HOST_USER = 'address.0@gmail.com'
+EMAIL_HOST_PASSWORD = '****'
