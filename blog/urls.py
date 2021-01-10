e37cecc0807f5e50257989bcf11a124912f3e827
jeddou <jeddou.ehtp@gmail.com>
2021-01-10 14:15:32 +0100

    add  PostUpdateView

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     PostListView,
     PostDetailView,
-    PostCreateView
+    PostCreateView,PostUpdateView
    )
 from . import views
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
 
 
     path('about/', views.about, name='blog-about'),
